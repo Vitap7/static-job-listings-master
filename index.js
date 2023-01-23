953151da86f3b89a931ca4e0f20db7877a678be0
Vitap7 <aylsn123@qq.com>
2023-01-24 00:02:14 +0800

    feat: add partial css 2

--- index.js
+++ index.js
@@ -8,26 +8,27 @@ fetch("data.json")
         <div class="middle-info">
           <header>
             <h3>${_.company}</h3>
-            <p>
-              ${_.new ? `<span class="span-new">NEW!</span>` : ""}${
+            <p>${_.new ? `<span class="span-new">NEW!</span>` : ""}${
         _.featured ? `<span class="span-featured">FEATURED</span>` : ""
-      }
-            </p>
+      }</p>
           </header>
 
           <h2>${_.position}</h2>
           <div class="detail-info">
             <p>${_.postedAt}</p>
+            <p>·</p>
             <p>${_.contract}</p>
+            <p>·</p>
             <p>${_.location}</p>
           </div>
         </div>
-        <p>
-          <span>${_.role}</span><span>${_.level}</span>${_.languages
+
+        <div class="job-tags">
+          <p>${_.role}</p><p>${_.level}</p>${_.languages
         .concat(_.tools)
-        .map((_) => `<span>${_}</span>`)
+        .map((_) => `<p>${_}</p>`)
         .join("")}
-        </p>
+        </div>
       </section>
     </li>`
     );
