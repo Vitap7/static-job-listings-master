d5006703ae68365dc6e8e5a46b75e80979770572
Vitap7 <aylsn123@qq.com>
2023-01-24 11:42:27 +0800

    feat: toggle tags

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ fetch("data.json")
     const list = document.querySelector(".job-list");
     list.innerHTML = jobs.join("");
 
+    const filterTags = new Set();
+
     function toggleChosen(e) {
-      e.target.classList.toggle("chosen");
+      const result = e.target.classList.toggle("chosen");
+      const tagText = e.target.textContent;
+
+      if (result) {
+        filterTags.add(tagText);
+        tags.forEach((tag) => {
+          if (tagText === tag.textContent) {
+            tag.classList.add("chosen");
+          }
+        });
+      } else {
+        filterTags.delete(tagText);
+        tags.forEach((tag) => {
+          if (tagText === tag.textContent) {
+            tag.classList.remove("chosen");
+          }
+        });
+      }
+      console.log(filterTags);
     }
 
     const tags = document.querySelectorAll(".job-tags p");
